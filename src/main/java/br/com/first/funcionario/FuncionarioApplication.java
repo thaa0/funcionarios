7cf6e4f805b10e574287d37062aaafa03f642451
Thalita Souza <dev.thalita@gmail.com>
2024-09-04 17:29:16 -0300

    implementa api de teste

--- src/main/java/br/com/first/funcionario/FuncionarioApplication.java
+++ src/main/java/br/com/first/funcionario/FuncionarioApplication.java
@@ -2,9 +2,19 @@ package br.com.first.funcionario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class FuncionarioApplication {
+	
+	@GetMapping
+	public String getHome() {
+		return "Gestao Funcionarios - API home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FuncionarioApplication.class, args);
