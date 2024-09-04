75c3ea735afe4d61be83cdc15785057088fd697d
Thalita Souza <dev.thalita@gmail.com>
2024-09-03 21:38:53 -0300

    inicia projeto

---
+++ src/test/java/br/com/first/funcionario/FuncionarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.first.funcionario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuncionarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
