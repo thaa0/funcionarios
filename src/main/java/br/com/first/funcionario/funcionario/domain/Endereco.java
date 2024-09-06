d3f8f138bcad3919dce8848b5f440c40e17f3385
Thalita Souza <dev.thalita@gmail.com>
2024-09-05 23:06:57 -0300

    implementa validacao notblanknos campos de endereco

--- src/main/java/br/com/first/funcionario/funcionario/domain/Endereco.java
+++ src/main/java/br/com/first/funcionario/funcionario/domain/Endereco.java
@@ -1,6 +1,8 @@
 package br.com.first.funcionario.funcionario.domain;
 
 import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,10 +10,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class Endereco {
+	@NotBlank(message = "Este campo não pode estar em branco")
 	private String rua;
+	@NotBlank(message = "Este campo não pode estar em branco")
 	private String bairro;
+	@NotBlank(message = "Este campo não pode estar em branco")
     private String cidade;
+	@NotBlank(message = "Este campo não pode estar em branco")
     private String estado;
+	@NotBlank(message = "Este campo não pode estar em branco")
     private String cep;
 
     public Endereco(String rua, String bairro, String cidade, String estado, String cep) {
